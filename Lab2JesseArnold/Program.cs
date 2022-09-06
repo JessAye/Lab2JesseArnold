b76ac6b54323ac31171f66e951a0ceea419eb7c3
Jesse Arnold <ARNOLDJT@ETSU.EDU>
2022-09-06 16:28:38 -0400

    updating the dev

--- Lab2JesseArnold/Program.cs
+++ Lab2JesseArnold/Program.cs
@@ -1,13 +1,22 @@
-﻿
-namespace Program
+﻿namespace Program
 {
     class addition
     {
-        static void main(string[] args)
+        static void Main(string[] args)
         {
+            var firstNumber = 0;
+            var secondNumber = 0;
+            
+            
             Console.WriteLine("Enter 2 Numbers and they will be added together");
+            Console.WriteLine("First Number?: ");
+            firstNumber = Int32.Parse(Console.ReadLine()!);
+            Console.WriteLine("Second Number?: ");
+            secondNumber = Int32.Parse(Console.ReadLine()!);
+            Console.WriteLine(string.Format("{0} + {1} = {2}",firstNumber,secondNumber,firstNumber+secondNumber));
         }
+
+      
     }
 
 }
-
