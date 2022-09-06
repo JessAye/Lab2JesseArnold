f123d21cb0daa9c425f8c2d6767d8fb458da8d3f
Jesse Arnold <ARNOLDJT@ETSU.EDU>
2022-09-06 15:50:49 -0400

    adding the skeleton code

--- Lab2JesseArnold/Program.cs
+++ Lab2JesseArnold/Program.cs
@@ -1,3 +1,13 @@
 ﻿
+namespace Program
+{
+    class addition
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Enter 2 Numbers and they will be added together");
+        }
+    }
 
-Console.WriteLine("Github test");
+}
+
